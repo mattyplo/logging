03879d007af285c449eda374bba10796adb7f6ec
mattyplo <matthew.ploszaj@seattlecentral.edu>
2019-11-13 10:29:48 -0800

    created method to read file

--- main.py
+++ main.py
@@ -6,11 +6,24 @@ def countImperdietInSentence(sentence):
       count += 1
   return count
 
-fileSource = input("What is the destination of the file you'd like to read?")
-file = open(fileSource, "r")
-fileText = file.read()
-sentences = fileText.split(".")
-for sentence in sentences:
-  print(countImperdietInSentence(sentence))
+def imperdietIsInSentence(count):
+  return count > 0
+
+def readFile():
   
-      
+  fileSource = input("What is the destination of the file you'd like to read?")
+  file = open(fileSource, "r")
+  fileText = file.read()
+  sentences = fileText.split(".")
+  numLinesWithImperdiet = 0
+  numImperdietInFile = 0
+  for sentence in sentences:
+    numImperdietInSentence = countImperdietInSentence(sentence)
+    numImperdietInFile += numImperdietInSentence
+    if numImperdietInSentence > 0:
+      numLinesWithImperdiet += 1
+
+  print("number of lines with imperdiet: ", numLinesWithImperdiet)
+  print("number of times imperdiet appears: ", numImperdietInFile)
+  
+readFile()      
